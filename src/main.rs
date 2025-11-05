0fa7b2154bda4794ac7884a7494397a79b6f629f
Simon Cozens <simon@simon-cozens.org>
2025-11-05 21:16:19 +0000

    Help user with extensions

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,33 @@ struct Cli {
     output: std::path::PathBuf,
 }
 
+const SUPPORTED_EXTENSIONS: &[&str; 5] =
+    &["ufo", "designspace", "glyphs", "glyphspackage", "babelfont"];
+
 fn main() {
     let args = Cli::parse();
     env_logger::init();
+    if !SUPPORTED_EXTENSIONS.contains(&args.font_path.extension().unwrap().to_str().unwrap()) {
+        log::error!(
+            "Input extension {:?} is not in the list of supported extensions: {:?}",
+            args.font_path.extension().unwrap(),
+            SUPPORTED_EXTENSIONS
+        );
+        std::process::exit(1);
+    }
+    if !SUPPORTED_EXTENSIONS.contains(&args.output.extension().unwrap().to_str().unwrap()) {
+        log::error!(
+            "Output extension {:?} is not in the list of supported extensions: {:?}",
+            args.output.extension().unwrap(),
+            SUPPORTED_EXTENSIONS
+        );
+        std::process::exit(1);
+    }
     println!(
         "Converting font at {:?} to {:?}",
         args.font_path, args.output
     );
+
     let input = babelfont::load(args.font_path).expect("Failed to load font");
     input.save(args.output).expect("Failed to save font");
 }
