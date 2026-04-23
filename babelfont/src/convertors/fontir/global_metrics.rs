f709343d8608a2d0a460129205e5de2b459be9fd
Simon Cozens <simon@simon-cozens.org>
2026-04-23 16:24:25 +0100

    Move more metrics

--- babelfont/src/convertors/fontir/global_metrics.rs
+++ babelfont/src/convertors/fontir/global_metrics.rs
@@ -83,73 +83,71 @@ impl Work<Context, WorkId, Error> for GlobalMetricWork {
             metrics.set_if_some(GlobalMetric::CapHeight, pos.clone(), Some(cap_height));
             metrics.set_if_some(GlobalMetric::XHeight, pos.clone(), Some(x_height));
 
-            // // Some .glyphs files have a negative win descent so abs()
-            // // we don't use the macro here because of the special abs() logic
-            // metrics.set_if_some(
-            //     GlobalMetric::Os2WinDescent,
-            //     pos.clone(),
-            //     master
-            //         .custom_parameters
-            //         .win_descent
-            //         .or(font.custom_parameters.win_descent)
-            //         .map(|v| v.abs() as f64),
-            // );
+            // Some .glyphs files have a negative win descent so abs()
+            // we don't use the macro here because of the special abs() logic
+            metrics.set_if_some(
+                GlobalMetric::Os2WinDescent,
+                pos.clone(),
+                master
+                    .metrics
+                    .get(&MetricType::WinDescent)
+                    .map(|v| v.abs() as f64),
+            );
 
-            // macro_rules! set_metric {
-            //     // the most common pattern
-            //     ($variant:ident, $field_name:ident) => {
-            //         set_metric!(
-            //             $variant,
-            //             master
-            //                 .custom_parameters
-            //                 .$field_name
-            //                 .or(font.custom_parameters.$field_name)
-            //                 .map(|v| v as f64)
-            //         )
-            //     };
-            //     // a few fields have a manual default though
-            //     ($variant:ident, $field_name:ident, $fallback:literal) => {
-            //         set_metric!(
-            //             $variant,
-            //             master
-            //                 .custom_parameters
-            //                 .$field_name
-            //                 .or(font.custom_parameters.$field_name)
-            //                 .or(Some($fallback.into()))
-            //         )
-            //     };
-            //     // base case, both branches above resolve to this
-            //     ($variant:ident, $getter:expr ) => {
-            //         metrics.set_if_some(GlobalMetric::$variant, pos.clone(), $getter)
-            //     };
-            // }
-            // set_metric!(Os2TypoAscender, typo_ascender);
-            // set_metric!(Os2TypoDescender, typo_descender);
-            // set_metric!(Os2TypoLineGap, typo_line_gap);
-            // set_metric!(Os2WinAscent, win_ascent);
-            // set_metric!(StrikeoutPosition, strikeout_position);
-            // set_metric!(StrikeoutSize, strikeout_size);
-            // set_metric!(SubscriptXOffset, subscript_x_offset);
-            // set_metric!(SubscriptXSize, subscript_x_size);
-            // set_metric!(SubscriptYOffset, subscript_y_offset);
-            // set_metric!(SubscriptYSize, subscript_y_size);
-            // set_metric!(SuperscriptXOffset, superscript_x_offset);
-            // set_metric!(SuperscriptXSize, superscript_x_size);
-            // set_metric!(SuperscriptYOffset, superscript_y_offset);
-            // set_metric!(SuperscriptYSize, superscript_y_size);
-            // set_metric!(HheaAscender, hhea_ascender);
-            // set_metric!(HheaDescender, hhea_descender);
-            // set_metric!(HheaLineGap, hhea_line_gap);
-            // set_metric!(CaretSlopeRun, hhea_caret_slope_run);
-            // set_metric!(CaretSlopeRise, hhea_caret_slope_rise);
-            // set_metric!(CaretOffset, hhea_caret_offset);
-            // // 50.0 is the Glyphs default <https://github.com/googlefonts/glyphsLib/blob/9d5828d874110c42dfc5f542db8eb84f88641eb5/Lib/glyphsLib/builder/custom_params.py#L1136-L1156>
-            // set_metric!(UnderlineThickness, underline_thickness, 50.0);
-            // // -100.0 is the Glyphs default <https://github.com/googlefonts/glyphsLib/blob/9d5828d874110c42dfc5f542db8eb84f88641eb5/Lib/glyphsLib/builder/custom_params.py#L1136-L1156>
-            // set_metric!(UnderlinePosition, underline_position, -100.0);
-            // set_metric!(VheaCaretSlopeRise, vhea_caret_slope_rise);
-            // set_metric!(VheaCaretSlopeRun, vhea_caret_slope_run);
-            // set_metric!(VheaCaretOffset, vhea_caret_offset);
+            macro_rules! set_metric {
+                // the most common pattern
+                ($variant:ident, $field_name:ident) => {
+                    set_metric!(
+                        $variant,
+                        master
+                            .metrics
+                            .get(&MetricType::$field_name)
+                            .map(|v| *v as f64)
+                    )
+                };
+                // a few fields have a manual default though
+                ($variant:ident, $field_name:ident, $fallback:literal) => {
+                    set_metric!(
+                        $variant,
+                        master
+                            .metrics
+                            .get(&MetricType::$field_name)
+                            .map(|v| *v as f64)
+                            .or(Some($fallback.into()))
+                    )
+                };
+                // base case, both branches above resolve to this
+                ($variant:ident, $getter:expr ) => {
+                    metrics.set_if_some(GlobalMetric::$variant, pos.clone(), $getter)
+                };
+            }
+            set_metric!(Os2TypoAscender, TypoAscender);
+            set_metric!(Os2TypoDescender, TypoDescender);
+            set_metric!(Os2TypoLineGap, TypoLineGap);
+            set_metric!(Os2WinAscent, WinAscent);
+            set_metric!(StrikeoutPosition, StrikeoutPosition);
+            set_metric!(StrikeoutSize, StrikeoutSize);
+            set_metric!(SubscriptXOffset, SubscriptXOffset);
+            set_metric!(SubscriptXSize, SubscriptXSize);
+            set_metric!(SubscriptYOffset, SubscriptYOffset);
+            set_metric!(SubscriptYSize, SubscriptYSize);
+            set_metric!(SuperscriptXOffset, SuperscriptXOffset);
+            set_metric!(SuperscriptXSize, SuperscriptXSize);
+            set_metric!(SuperscriptYOffset, SuperscriptYOffset);
+            set_metric!(SuperscriptYSize, SuperscriptYSize);
+            set_metric!(HheaAscender, HheaAscender);
+            set_metric!(HheaDescender, HheaDescender);
+            set_metric!(HheaLineGap, HheaLineGap);
+            set_metric!(CaretSlopeRun, HheaCaretSlopeRun);
+            set_metric!(CaretSlopeRise, HheaCaretSlopeRise);
+            set_metric!(CaretOffset, HheaCaretOffset);
+            // 50.0 is the Glyphs default <https://github.com/googlefonts/glyphsLib/blob/9d5828d874110c42dfc5f542db8eb84f88641eb5/Lib/glyphsLib/builder/custom_params.py#L1136-L1156>
+            set_metric!(UnderlineThickness, UnderlineThickness, 50.0);
+            // -100.0 is the Glyphs default <https://github.com/googlefonts/glyphsLib/blob/9d5828d874110c42dfc5f542db8eb84f88641eb5/Lib/glyphsLib/builder/custom_params.py#L1136-L1156>
+            set_metric!(UnderlinePosition, UnderlinePosition, -100.0);
+            // set_metric!(VheaCaretSlopeRise, VheaCaretSlopeRise);
+            // set_metric!(VheaCaretSlopeRun, VheaCaretSlopeRun);
+            // set_metric!(VheaCaretOffset, VheaCaretOffset);
 
             // // https://github.com/googlefonts/glyphsLib/blob/c4db6b981d577f456d64ebe9993818770e170454/Lib/glyphsLib/builder/masters.py#L74-L92
             // metrics.set(
